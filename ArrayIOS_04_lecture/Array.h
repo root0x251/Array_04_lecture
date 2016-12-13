d93286e42e323f434182397ad6e6fcc61e9eda88
Slava <rtootik@gmail.com>
2016-12-14 00:46:12 +0300

    create

---
+++ ArrayIOS_04_lecture/Array.h
@@ -0,0 +1,13 @@
+//
+//  Array.h
+//  ArrayIOS_04_lecture
+//
+//  Created by Slava on 14.12.16.
+//  Copyright © 2016 Slava. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Array : NSObject
+
+@end
