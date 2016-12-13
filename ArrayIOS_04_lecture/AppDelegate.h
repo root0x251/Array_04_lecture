05a761e9d06b632eccba89e1e419799337839c7e
Slava <rtootik@gmail.com>
2016-12-14 00:26:59 +0300

    Initial Commit

---
+++ ArrayIOS_04_lecture/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ArrayIOS_04_lecture
+//
+//  Created by Slava on 14.12.16.
+//  Copyright © 2016 Slava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ArrayIOS_04_lecture/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ArrayIOS_04_lecture
+//
+//  Created by Slava on 14.12.16.
+//  Copyright © 2016 Slava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
